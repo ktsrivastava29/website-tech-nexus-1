cc101e038d7b26d260be28b1d91c17249d7699c4
Vishwjeet Singh <vishwjeetsingh594@gmail.com>
2021-10-31 14:20:30 +0530

    component folder and loading page added

--- client/src/Components/Loadingpage/Loading.js
+++ client/src/Components/Loadingpage/Loading.js
@@ -7,6 +7,7 @@ const loading = ()=>{
             <div className={classes.videobox}>
                 <video 
                 src={video} 
+                
                 autoPlay={true}
                 muted
                 loop></video>
