8692b7d071483b90d0c450c1ab5647e71bb88bc6
arora-jasjot <jasjotsinghchugh@gmail.com>
2021-11-01 19:32:34 +0530

    Added Logo

--- client/src/Components/Home/Navbar/Navbar.js
+++ client/src/Components/Home/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import image from '../Media/White.svg'
 
 import './Navbar.css';
 
@@ -6,7 +7,7 @@ const Navbar = () => {
     return (
         <nav>
             <p>Tech</p>
-            <div id="logo"></div>
+            <div id="logo"><img src={image} alt="" /></div>
             <p>Nexus</p>
         </nav>
     )
