049911d4aa7b216b15f0f3a6cbbcf3cf4e781c2c
Arnav Sharma <79043654+Arnav-Sharmaa@users.noreply.github.com>
2021-11-01 12:39:56 +0530

    Add files via upload

---
+++ client/src/Components/Sponsors/Sponsors.js
@@ -0,0 +1,325 @@
+import React from "react";
+import "./sponsors.css";
+import image from "./images/GFG.png";
+
+const Sponsor = () => {
+    return (
+        <>
+            <div className="bd">
+                <h1>SPONSORS</h1>
+                <div className="container">
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box" id="blank"></div>
+                    <div className="box" id="blank"></div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box" id="blank"></div>
+                    <div className="box" id="blank"></div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box" id="blank"></div>
+                    <div className="box" id="blank"></div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box" id="blank"></div>
+                    <div className="box" id="blank"></div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="box">
+                        <a href="https://www.geeksforgeeks.org/">
+                            <div className="front">
+                                <img src={image} />
+                            </div>
+                            <div className="back">
+                                <div className="name">
+                                    Geek for geeks<br/>
+                                    <span className="Type">
+                                        <i>(Educational sponsor)</i>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Sponsor;
