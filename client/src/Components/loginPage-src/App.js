bcff3e9330dee1423daff4823f5c4d46e2ea32f6
Tanmay Srivastava <ktsrivastava29@gmail.com>
2021-11-01 19:51:43 +0530

    added login page

---
+++ client/src/Components/loginPage-src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./components/Login"
+import bg from './bg-login.png';
+
+function App() {
+  return (
+    <div className="App">
+     
+   <Login />
+    </div>
+  );
+}
+
+export default App;
