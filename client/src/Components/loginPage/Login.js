544a0d4db99368129e8a8e7c973adf29169927bf
Tanmay Srivastava <ktsrivastava29@gmail.com>
2021-11-08 14:26:46 +0530

    added component

--- client/src/Components/loginPage/Login.js
+++ client/src/Components/loginPage/Login.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import backgroundVideo from './Assets/bg.mp4'
 import "./Login.css"
 
 function Login() {
   return(
-   
+   <>
+   <video autoPlay loop muted id='video'>
+       <source src={backgroundVideo} type='video/mp4'/>
+     </video>
   <div id="loginform">
 
   <FormHeader title="LOG IN" />
   <Form />
   <OtherMethods />
-</div>
+</div></>
 );
 }
 export default Login;
