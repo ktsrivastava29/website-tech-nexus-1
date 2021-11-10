e461c3f9321306a455444799cc242d8b077a2679
Vishwjeet Singh <vishwjeetsingh594@gmail.com>
2021-11-10 11:02:09 +0530

    updated

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Navbar from "../Navbar/Navbar"
 import "./Home.css"
 
-export default function Home () {
+export default function Home (props) {
+    // clickedhome ={()=>settorender('Home Page')}
+    //    clickedaboutus = {()=>{settorender('About us')}}
+    //    clickedcompetitionspage = {()=>{settorender('Competition Page')}}
+    //    clickedsponsor ={()=>{settorender('Sponsor')}}
     return (
         <>
-            <Navbar />
+            <Navbar clickedaboutus = {props.clickedaboutus} clickedcompetitionpage = {props.clickedcompetitionspage} clickedsponsor = {props.clickedsponsor} />
             <div id="home">
                 <div id="tagline">
                     <h1>Meeting <span>the</span> ExtraOrdinary</h1>
--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -2,21 +2,26 @@ import React from 'react'
 import './Navbar.css';
 import { BsList } from 'react-icons/bs';
 import image from './White.svg'
+import { PinDropSharp } from '@material-ui/icons';
 
 
-export default function Navbar() {
+export default function Navbar(props) {
     const expand = () =>{
         const navList = document.querySelector("#navList");
         if(navList.style.height !== "60vh") navList.style.height = "60vh";
         else navList.style.height = "0vh";
     }
+    // clickedhome ={()=>settorender('Home Page')}
+    //    clickedaboutus = {()=>{settorender('About us')}}
+    //    clickedcompetitionspage = {()=>{settorender('Competition Page')}}
+    //    clickedsponsor ={()=>{settorender('Sponsor')}}
     return (
         <nav>
             <div id="logo">Tech <img src={image} alt="" /> Nexus</div>
             <div id="navList" className="">
-                <div onClick = {expand} >Events</div>
-                <div onClick = {expand} >About</div>
-                <div onClick = {expand} >Sponsors</div>
+                <div onClick = {props.clickedcompetitionpage} >Events</div>
+                <div onClick = {props.clickedaboutus} >About</div>
+                <div onClick = {props.clickedsponsor} >Sponsors</div>
                 <div onClick = {expand} id="res-contact">Contact Us</div>
             </div>
             <div id="contactBtn">Contact Us</div>
