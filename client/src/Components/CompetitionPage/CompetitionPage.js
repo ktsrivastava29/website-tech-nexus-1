8956a7fc28b85aad1e61ca7a15296f55d149a175
Arjun Arora <arjunarora.aa.aa@gmail.com>
2021-11-10 16:49:49 +0530

    Linked competition detail page

--- client/src/Components/CompetitionPage/CompetitionPage.js
+++ client/src/Components/CompetitionPage/CompetitionPage.js
@@ -1,17 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "3d-react-carousal";
 import "./competitionPage.css";
-import { bounceIn } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
-const clicked = (id) => {
-  console.log("Id " + id + " Clicked");
-};
+import { bounceIn } from "react-animations";
+import Radium, { StyleRoot } from "radium";
+import CompetitionDetail from "./CompetitionDetail";
+import hackathonPoster from "./assets/hackathon.jpg";
 
 let slides = [
-  <img
-    src="https://previews.123rf.com/images/vasilyrosca/vasilyrosca1710/vasilyrosca171000084/88190043-electro-party-music-night-poster-template-electro-style-concert-disco-party-event-invitation-.jpg"
-    alt="1"
-  />,
+  <img src={hackathonPoster} alt="" />,
   <img
     src="https://previews.123rf.com/images/vasilyrosca/vasilyrosca1710/vasilyrosca171000084/88190043-electro-party-music-night-poster-template-electro-style-concert-disco-party-event-invitation-.jpg"
     alt="2"
@@ -29,35 +25,49 @@ let slides = [
     alt="5"
   />,
 ];
-const images = slides.map((slide) => {
-  console.log(slide.props.src);
-  return (
-    <>
-      <div key={slide.props.alt} className="competitionImage">
-        <img
-          src={slide.props.src}
-          alt="An imag"
-          onClick={() => clicked(slide.props.alt)}
-        />
-      </div>
-    </>
-  );
-});
 
 const CompetitionPage = () => {
+  const [torender, settorender] = useState("CompetitionPage");
+
+  const openEvent = (id) => {
+    console.log(id);
+    settorender("CompetitionDetail");
+  };
+
+  const images = slides.map((slide) => {
+    console.log(slide.props.src);
+    return (
+      <>
+        <div key={slide.props.alt} className="competitionImage">
+          <img
+            src={slide.props.src}
+            alt="An imag"
+            onClick={() => openEvent(slide.props.alt)}
+          />
+        </div>
+      </>
+    );
+  });
+
   const styles = {
     bounceIn: {
-      animation: 'x 1s',
-      animationName: Radium.keyframes(bounceIn, 'bounceIn')
-    }
-  }
+      animation: "x 1s",
+      animationName: Radium.keyframes(bounceIn, "bounceIn"),
+    },
+  };
   return (
-    <StyleRoot>
-    <div id="competitions" style={styles.bounceIn}>
-      <div className="headingLabel">COMPETITIONS</div>
-      <Carousel slides={images} autoplay={false} />
-    </div>
-    </StyleRoot>
+    <>
+      {torender === "CompetitionPage" ? (
+        <StyleRoot>
+          <div id="competitions" style={styles.bounceIn}>
+            <div className="headingLabel">COMPETITIONS</div>
+            <Carousel slides={images} autoplay={false} />
+          </div>
+        </StyleRoot>
+      ) : (
+        <CompetitionDetail />
+      )}
+    </>
   );
 };
 
