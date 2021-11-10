ac7ad3dd3dbc49da05e6cbe02edf9f3eaa2018b4
Arjun Arora <arjunarora.aa.aa@gmail.com>
2021-11-10 19:52:41 +0530

    Added data updated Competition page and detail

--- client/src/Components/CompetitionPage/CompetitionDetail.js
+++ client/src/Components/CompetitionPage/CompetitionDetail.js
@@ -5,9 +5,10 @@ import polygon from "./assets/polygon.png";
 import celo from "./assets/celo.png";
 import filecoin from "./assets/filecoin.png";
 import devfolio from "./assets/devfolio.png";
-import hackathonPoster from "./assets/hackathon.jpg";
 
-const CompetitionDetail = () => {
+const CompetitionDetail = (data) => {
+  const { id, heading, subHeading, image, content } = data.data;
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://apply.devfolio.co/v2/sdk.js";
@@ -21,63 +22,55 @@ const CompetitionDetail = () => {
 
   return (
     <>
-      <div id="aboutCompetition">
-        <div className="aboutCompetitionHeading">HACKATHON</div>
-        <div className="aboutCompetitionDetails">
-          <div className="details">
-            <p>
-              Introducing "Vividhata: A blend of ideas," a three-day National
-              Level Hackathon, and "Imprenditoria: Innovation on Point," an idea
-              incubation event where participants would solve real-world
-              industrial challenges. The problem statements would be made
-              public, which would have ramifications across all engineering
-              fields. Knowing that creativity has no bounds, we devised 5-5
-              real-world problems for the hackathon and idea incubation to
-              provide a space for all technical professionals to let their
-              creative ideas flow! Creativity is the new currency in town, thus
-              new enterprises with novel ideas are welcomed. In an open
-              exhibition, the hackathon models and ideas will be shown, and we
-              will have our winners. So be ready, for a history of creativity is
-              waiting for you to extend your horizons by providing solutions to
-              some of the world's most challenging problems.
-            </p>
-            <div className="button">
-              <div
-                className="apply-button"
-                data-hackathon-slug="vividhata-a-blend-of-ideas"
-                data-button-theme="dark-inverted"
-              ></div>
+      {id === 1 ? (
+        <div id="aboutCompetition">
+          <div className="aboutCompetitionHeading">
+            {heading}
+            <br /> <p>{subHeading}</p>
+          </div>
+          <div className="aboutCompetitionDetails">
+            <div className="details">
+              <p>{content}</p>
+              <div className="button">
+                <div
+                  className="apply-button"
+                  data-hackathon-slug="vividhata-a-blend-of-ideas"
+                  data-button-theme="dark-inverted"
+                ></div>
+              </div>
+            </div>
+            <div className="image">
+              <img src={image} alt="delails" />
             </div>
           </div>
-          <div className="image">
-            <img src={hackathonPoster} alt="delails" />
-          </div>
-        </div>
-        <div className="sponsors">
-          <h4>Golden Sponsor</h4>
+          <div className="sponsors">
+            <h4>Golden Sponsor</h4>
 
-          <div className="gold">
-            <a href="https://devfolio.co/home/" target="_blank">
-              <img src={devfolio} alt="" />
-            </a>
-          </div>
-          <h5>Silver Sponsors</h5>
-          <div className="silver">
-            <a href="https://tezos.com/" target="_blank">
-              <img src={tezos} alt="" />
-            </a>
-            <a href="https://filecoin.io/" target="_blank">
-              <img src={filecoin} alt="" />
-            </a>
-            <a href="https://polygon.technology/" target="_blank">
-              <img src={polygon} alt="" />
-            </a>
-            <a href="https://celo.org/" target="_blank">
-              <img src={celo} alt="" />
-            </a>
+            <div className="gold">
+              <a href="https://devfolio.co/home/" target="_blank">
+                <img src={devfolio} alt="" />
+              </a>
+            </div>
+            <h5>Silver Sponsors</h5>
+            <div className="silver">
+              <a href="https://tezos.com/" target="_blank">
+                <img src={tezos} alt="" />
+              </a>
+              <a href="https://filecoin.io/" target="_blank">
+                <img src={filecoin} alt="" />
+              </a>
+              <a href="https://polygon.technology/" target="_blank">
+                <img src={polygon} alt="" />
+              </a>
+              <a href="https://celo.org/" target="_blank">
+                <img src={celo} alt="" />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <h1> Coming Soon! stay tuned</h1>
+      )}
     </>
   );
 };
